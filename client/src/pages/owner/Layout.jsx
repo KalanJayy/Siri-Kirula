641715199b94d94080b841f5bcf3889e33679acb
Kalana Jayaweera <kalana33471@gmail.com>
2025-07-04 17:59:31 +0530

    updated navbar

--- client/src/pages/owner/Layout.jsx
+++ client/src/pages/owner/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import NavBarOwner from '../../components/owner/NavBarOwner'
+import NavBarOwner from '../../components/owner/NavbarOwner'
 import Sidebar from '../../components/owner/Sidebar'
 import { Outlet } from 'react-router-dom'
 import { useAppContext } from '../../context/AppContext'
